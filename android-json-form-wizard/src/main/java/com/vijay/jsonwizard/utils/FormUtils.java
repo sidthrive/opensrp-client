8eaab01022c1a1064eb3b1dcad563dbe02ed260b
Jason Rogena <jasonrogena@gmail.com>
2017-02-23 16:22:14 +0300

    JR| Meta Time Format in Native Forms
    Update the time format in native forms to yyyy-MM-dd HH:mm:ss
    
    Signed-off-by: Jason Rogena <jasonrogena@gmail.com>

--- android-json-form-wizard/src/main/java/com/vijay/jsonwizard/utils/FormUtils.java
+++ android-json-form-wizard/src/main/java/com/vijay/jsonwizard/utils/FormUtils.java
@@ -26,7 +26,7 @@ public class FormUtils {
     public static final int MATCH_PARENT = -1;
     public static final int WRAP_CONTENT = -2;
     public static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd-MM-yyyy");
-    public static final SimpleDateFormat DATE_TIME_FORMAT = new SimpleDateFormat("yyy-MM-dd'T'HH:mm:ssZ");
+    public static final SimpleDateFormat DATE_TIME_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
     private static final String METADATA_PROPERTY = "metadata";
     private static final String START_JAVAROSA_PROPERTY = "start";
     private static final String END_JAVAROSA_PROPERTY = "end";
