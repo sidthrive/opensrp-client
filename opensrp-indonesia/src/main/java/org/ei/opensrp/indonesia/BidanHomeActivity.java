1b0a0e31a81c17cca1cef856b9c416ceddf075a4
MarwanNulhakam <atosoft.null@gmail.com>
2017-03-24 06:46:31 +0800

    MN | put kia report site to home dashboard

--- opensrp-indonesia/src/main/java/org/ei/opensrp/indonesia/BidanHomeActivity.java
+++ opensrp-indonesia/src/main/java/org/ei/opensrp/indonesia/BidanHomeActivity.java
@@ -119,7 +119,7 @@ public class BidanHomeActivity extends SecuredActivity {
         FlurryAgent.logEvent("home_dashboard",Home, true );
 
         setContentView(R.layout.smart_registers_home_bidan);
-        navigationController = new NavigationControllerINA(this,anmController);
+        navigationController = new NavigationControllerINA(this,anmController,context());
         setupViews();
         initialize();
         DisplayFormFragment.formInputErrorMessage = getResources().getString(R.string.forminputerror);
@@ -137,7 +137,7 @@ public class BidanHomeActivity extends SecuredActivity {
 
 
         findViewById(R.id.btn_reporting).setOnClickListener(onButtonsClickListener);
-        findViewById(R.id.btn_videos).setOnClickListener(onButtonsClickListener);
+//        findViewById(R.id.btn_videos).setOnClickListener(onButtonsClickListener);
 
         ecRegisterClientCountView = (TextView) findViewById(R.id.txt_kartu_ibu_register_client_count);
         kartuIbuANCRegisterClientCountView = (TextView) findViewById(R.id.txt_kartu_ibu_anc_register_client_count);
@@ -339,12 +339,12 @@ public class BidanHomeActivity extends SecuredActivity {
         public void onClick(View view) {
             switch (view.getId()) {
                 case R.id.btn_reporting:
-//                    navigationController.startReports();
+                    navigationController.startReports();
                     break;
 
-                case R.id.btn_videos:
+//                case R.id.btn_videos:
 //                    navigationController.startVideos();
-                    break;
+//                    break;
             }
         }
     };
