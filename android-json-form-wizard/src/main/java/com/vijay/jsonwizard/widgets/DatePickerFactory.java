bfe7a8b22647223c100fe2b4d6adf5b1428c28cd
Jason Rogena <jasonrogena@gmail.com>
2017-03-24 15:39:38 +0300

    JR| Native Form Text Size Issues
    Fix issues that came up due to increasing the font size in the native
    forms
    
    Part of fix for issue #428
    
    Signed-off-by: Jason Rogena <jasonrogena@gmail.com>

--- android-json-form-wizard/src/main/java/com/vijay/jsonwizard/widgets/DatePickerFactory.java
+++ android-json-form-wizard/src/main/java/com/vijay/jsonwizard/widgets/DatePickerFactory.java
@@ -45,7 +45,7 @@ public class DatePickerFactory implements FormWidgetFactory {
 
     private static final long DAY_MILLSECONDS = 86400000;
     public static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd-MM-yyyy");
-    public static final String DATE_FORMAT_REGEX = "(^(((0[1-9]|1[0-9]|2[0-8])[-](0[1-9]|1[012]))|((29|30|31)[-](0[13578]|1[02]))|((29|30)[-](0[4,6,9]|11)))[-](19|[2-9][0-9])\\d\\d$)|(^29[-]02[-](19|[2-9][0-9])(00|04|08|12|16|20|24|28|32|36|40|44|48|52|56|60|64|68|72|76|80|84|88|92|96)$)";
+    public static final String DATE_FORMAT_REGEX = "(^(((0[1-9]|1[0-9]|2[0-8])[-](0[1-9]|1[012]))|((29|30|31)[-](0[13578]|1[02]))|((29|30)[-](0[4,6,9]|11)))[-](19|[2-9][0-9])\\d\\d$)|(^29[-]02[-](19|[2-9][0-9])(00|04|08|12|16|20|24|28|32|36|40|44|48|52|56|60|64|68|72|76|80|84|88|92|96)$)|\\s*";
 
     @Override
     public List<View> getViewsFromJson(String stepName, final Context context, JsonFormFragment formFragment, JSONObject jsonObject,
