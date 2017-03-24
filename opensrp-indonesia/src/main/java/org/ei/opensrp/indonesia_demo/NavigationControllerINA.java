8d88f464e4924b262824f113cc0da7636db30395
MarwanNulhakam <atosoft.null@gmail.com>
2017-03-24 18:37:11 +0800

    MN | link report button to reporting dashboard

--- opensrp-indonesia/src/main/java/org/ei/opensrp/indonesia_demo/NavigationControllerINA.java
+++ opensrp-indonesia/src/main/java/org/ei/opensrp/indonesia_demo/NavigationControllerINA.java
@@ -2,14 +2,17 @@ package org.ei.opensrp.indonesia_demo;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 
 
+import org.ei.opensrp.Context;
 import org.ei.opensrp.indonesia_demo.anc.NativeKIANCSmartRegisterActivity;
 import org.ei.opensrp.indonesia_demo.child.NativeKIAnakSmartRegisterActivity;
 import org.ei.opensrp.indonesia_demo.kartu_ibu.NativeKISmartRegisterActivity;
 import org.ei.opensrp.indonesia_demo.kb.NativeKBSmartRegisterActivity;
 import org.ei.opensrp.indonesia_demo.pnc.NativeKIPNCSmartRegisterActivity;
 import org.ei.opensrp.view.controller.ANMController;
+import org.json.JSONObject;
 
 
 import static android.preference.PreferenceManager.getDefaultSharedPreferences;
@@ -17,11 +20,13 @@ import static android.preference.PreferenceManager.getDefaultSharedPreferences;
 public class NavigationControllerINA extends org.ei.opensrp.view.controller.NavigationController {
     private Activity activity;
     private ANMController anmController;
+    private Context context;
 
-    public NavigationControllerINA(Activity activity, ANMController anmController) {
+    public NavigationControllerINA(Activity activity, ANMController anmController, Context context) {
         super(activity,anmController);
         this.activity = activity;
         this.anmController = anmController;
+        this.context=context;
     }
     @Override
     public void startECSmartRegistry() {
@@ -50,6 +55,19 @@ public class NavigationControllerINA extends org.ei.opensrp.view.controller.Navi
     @Override
     public void startChildSmartRegistry() {
         activity.startActivity(new Intent(activity, NativeKIAnakSmartRegisterActivity.class));
+    }
+    @Override
+    public void startReports() {
+        String id, pass;
+        try{
+            id = new JSONObject(anmController.get()).get("anmName").toString();
+            pass = context.allSettings().fetchANMPassword();
+        }catch(org.json.JSONException ex){
+            id="noname";
+            pass="null";
+        }
+        String uri = "http://"+id+":"+pass+"@"+activity.getApplicationContext().getString(R.string.dho_site).replace("http://","");
+        activity.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(uri)));
     }
 
 }
