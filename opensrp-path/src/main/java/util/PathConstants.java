a7beaf926a3afd2734aadf099fcb496c5e451c64
Jason Rogena <jasonrogena@gmail.com>
2017-04-04 14:59:47 +0300

    JR| PathConstants in build.gradle File
    Move the constants in PathConstants to the build.gradle file
    
    Signed-off-by: Jason Rogena <jasonrogena@gmail.com>

--- opensrp-path/src/main/java/util/PathConstants.java
+++ opensrp-path/src/main/java/util/PathConstants.java
@@ -1,23 +1,24 @@
 package util;
 
 import org.ei.opensrp.AllConstants;
+import org.ei.opensrp.path.BuildConfig;
 
 /**
  * Created by coder on 2/14/17.
  */
-public class PathConstants extends AllConstants{
-    public static final String OPENMRS_URL="";
-    public static String openmrsUrl(){
-        String baseUrl= org.ei.opensrp.Context.getInstance().allSharedPreferences().fetchBaseURL("");
-        int lastIndex=baseUrl.lastIndexOf("/");
-        baseUrl=baseUrl.substring(0,lastIndex)+"/openmrs";
-        return OPENMRS_URL.isEmpty()|| OPENMRS_URL==null?baseUrl:OPENMRS_URL;
+public class PathConstants extends AllConstants {
+    public static final String OPENMRS_URL = BuildConfig.OPENMRS_URL;
+
+    public static String openmrsUrl() {
+        String baseUrl = org.ei.opensrp.Context.getInstance().allSharedPreferences().fetchBaseURL("");
+        int lastIndex = baseUrl.lastIndexOf("/");
+        baseUrl = baseUrl.substring(0, lastIndex) + "/openmrs";
+        return OPENMRS_URL.isEmpty() || OPENMRS_URL == null ? baseUrl : OPENMRS_URL;
     }
-    public static final String OPENMRS_IDGEN_URL = "/module/idgen/exportIdentifiers.form";
-    public static final int OPENMRS_UNIQUE_ID_INITIAL_BATCH_SIZE =250;
-    public static final int OPENMRS_UNIQUE_ID_BATCH_SIZE =100;
-    public static final int OPENMRS_UNIQUE_ID_SOURCE =3;
-    public static final int VACCINE_SYNC_TIME =24;
 
-
+    public static final String OPENMRS_IDGEN_URL = BuildConfig.OPENMRS_IDGEN_URL;
+    public static final int OPENMRS_UNIQUE_ID_INITIAL_BATCH_SIZE = BuildConfig.OPENMRS_UNIQUE_ID_INITIAL_BATCH_SIZE;
+    public static final int OPENMRS_UNIQUE_ID_BATCH_SIZE = BuildConfig.OPENMRS_UNIQUE_ID_BATCH_SIZE;
+    public static final int OPENMRS_UNIQUE_ID_SOURCE = BuildConfig.OPENMRS_UNIQUE_ID_SOURCE;
+    public static final int VACCINE_SYNC_TIME = BuildConfig.VACCINE_SYNC_TIME;
 }
