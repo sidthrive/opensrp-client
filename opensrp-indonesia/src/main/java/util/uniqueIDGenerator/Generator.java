8d0c78c74376a0692989c5ec426fc3b85968d818
mib.iqbal <mib.iqbaal@gmail.com>
2017-05-02 16:06:35 +0800

    MI | Update Unique id to dynamic url

--- opensrp-indonesia/src/main/java/util/uniqueIDGenerator/Generator.java
+++ opensrp-indonesia/src/main/java/util/uniqueIDGenerator/Generator.java
@@ -37,9 +37,18 @@ public class Generator {
     public static final int UNIQUE_ID_LIMIT = 5;
     public static final int UNIQUE_ID_LENGTH_REQUEST = 1;
 
-    public Generator(Context context, String username, String password){
+    /*public Generator(Context context, String username, String password){
         this.context=context;
         url = "http://202.169.244.198:8080/openmrs/module/idgen/exportIdentifiers.form?source=1"+
+                "&numberToGenerate="+Integer.toString(UNIQUE_ID_LENGTH_REQUEST)+
+                "&username="+username+
+                "&password="+password;
+    }*/
+    public Generator(Context context, String username, String password){
+        this.context=context;
+        String  DRISTHI_BASE_URL = context.configuration().dristhiBaseURL().replace("opensrp","openmrs");
+        url =   DRISTHI_BASE_URL+
+                "/module/idgen/exportIdentifiers.form?source=1"+
                 "&numberToGenerate="+Integer.toString(UNIQUE_ID_LENGTH_REQUEST)+
                 "&username="+username+
                 "&password="+password;
